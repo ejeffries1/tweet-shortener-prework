047e3ab814593e88e7a361a86177a1773d9dba90
Evan Jeffries <ejeffries14@gmail.com>
2021-05-19 07:33:09 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,21 @@
 # Write your code here.
+def dictionary
+  dictionary = {
+    "hello" => 'hi', "to, two, too" => '2', "for, four," => '4', 'be' => 'b', 'you' => 'u', "at" => "@",
+    "and" => "&"
+  }
+end
+
+def word_substituter(tweet_one)
+  
+ tweet_one.split(" ").collect do |words|
+   keys = dictionary[:keys]
+    if keys.include?(words)
+      words = dictionary[words]
+    else
+      words
+    end
+  end.join(" ")
+end
+
+
